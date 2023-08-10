fcc9a91ddcb85a2c7e7a9887102e5efe851de12f
szymon.zajaczkowski <szymon.zajaczkowski@ecx.io>
2023-08-10 09:03:28 +0200

    WIP

--- routes/index.tsx
+++ routes/index.tsx
@@ -16,8 +16,7 @@ export default function Home() {
           alt="the fresh logo: a sliced lemon dripping with juice"
         />
         <p class="my-6">
-          Welcome to `fresh`. Try updating this message in the
-          ./routes/index.tsx file, and refresh.
+         Suchary od Lucjana
         </p>
         <Counter count={count} />
       </div>
